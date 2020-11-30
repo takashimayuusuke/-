562c0de1edf9378e2086c90ec46c0d39bf65cdf6
takashimayuusuke <taka.z34.vq37@gmail.com>
2020-11-30 21:36:44 +0900

    バリデーションの設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permit_parameters, if: :devise_controller?
-  
+
   private
   def configure_permit_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
