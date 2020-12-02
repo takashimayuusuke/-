48d62d1137e470ace6328fbe951cab47905fad5d
takashimayuusuke <taka.z34.vq37@gmail.com>
2020-12-02 15:27:23 +0900

    空のメッセージが送信できないバリデーションの設定

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,8 @@ class Message < ApplicationRecord
   belongs_to :user
   # roomテーブルに対してのアソシエーション
   belongs_to :room
+
+  validates :content, presence: true
+    # 空の状態で保存できなくする
+# この原因は、バリデーションエラー発生時、問題のあったinput要素を囲うように、エラーメッセージ表示用のdiv要素がRailsによって生成され、CSSの指定に齟齬が発生するため。このdiv要素は、class属性にfield_with_errorsという値を持っている。
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -5,5 +5,6 @@ class Room < ApplicationRecord
   has_many :messages
 
   validates :name, presence: true
+  # 空の状態で保存できなくする
   
 end
