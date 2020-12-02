6be98231ea7fc8de93e3b932ab75060075f5a399
takashimayuusuke <taka.z34.vq37@gmail.com>
2020-12-02 21:57:42 +0900

    モデルテスト練習問題２まで終了

---
+++ spec/models/room_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe Room, type: :model do
+  describe '#create' do
+    before do
+      @room = FactoryBot.build(:room)
+    end
+
+    it "nameの値が存在すれば登録できること" do
+
+    end
+
+    it "nameが空では登録できないこと" do
+
+    end
+  end
+end
