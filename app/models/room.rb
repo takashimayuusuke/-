4d907bd66e5ea8d0875ca3a7e838ff8f27f5c5ef
takashimayuusuke <taka.z34.vq37@gmail.com>
2020-12-02 17:46:50 +0900

    チャットルーム削除機能の実装

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,8 +1,10 @@
 class Room < ApplicationRecord
   has_many :room_users
-  has_many :users, through: :room_users
+  has_many :users, through: :room_users, dependent: :destroy
+
+
   # messageテーブルに対してのアソシエーション
-  has_many :messages
+  has_many :messages, dependent: :destroy
 
   validates :name, presence: true
   # 空の状態で保存できなくする
